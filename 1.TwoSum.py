e5a3d92db4f8780f1bb99e72c5ba3535f595de18
IsaacStyle <isaac.opra@gmail.com>
2023-04-03 11:23:00 -0400

    two sum python

---
+++ 1.TwoSum.py
@@ -0,0 +1,6 @@
+def twoSum(self, nums, target):
+        map = {}
+        for i,num in enumerate(nums):
+            if target - num in map:
+                return [map[target - num],i]
+            map[num] = i
