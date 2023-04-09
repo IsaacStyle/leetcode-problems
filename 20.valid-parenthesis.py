29653d9206eb2604638f5c42c59de2e23e13bc03
IsaacStyle <isaac.opra@gmail.com>
2023-04-09 11:43:00 -0400

    valid parenthesis solution 1

--- 20.valid-parenthesis.py
+++ 20.valid-parenthesis.py
@@ -1,21 +1,29 @@
-def isValid(self, s):
+class Solution(object):
+    def isValid(self, s):
         map = {
             '(':0,
             '[':0,
             '{':0
         }
+        check = 0
         for i,char in enumerate(s):
-            
+            if len(s) % 2 != 0: return False
             if char in map:
                 map[char] += 1
-                print(map[char])
+                check += 1
             if char == ')':
+                check -= 1
                 map['('] -= 1
                 if map['('] < 0: return False
+                if s[i-1] in map and s[i-1] != '(': return False
             if char == ']':
+                check -= 1
                 map['['] -= 1
                 if map['['] < 0: return False
+                if s[i-1] in map and s[i-1] != '[': return False
             if char == '}':
+                check -= 1
                 map['{'] -= 1
                 if map['{'] < 0: return False
-        return True
+                if s[i-1] in map and s[i-1] != '{': return False
+        if check == 0: return True
